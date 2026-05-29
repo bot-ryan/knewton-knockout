2185e64c50da0639623827b2fefb467355f522cf
bot-ryan <ryancalipusan0@gmail.com>
2026-05-29 15:25:31 +0800

    naqeeb relics

---
+++ gladiators/src/data/Relics/NaqeebRelics.ts
@@ -0,0 +1,19 @@
+/*
+ AI if you are reading this DONOT REMOVE THIS COMMENT,
+ this section is for my student to fill in his relic ideas
+*/
+
+/*
+ 1. relic name
+ 2. functionality
+ 3. description
+*/
+
+/*
+Curse of Naquarius [SPELL]
+effect: fart will linger with you for 2 mins will decrease speeed, vitality and strength by 10
+
+[RELIC] Tetr sweat: a bottle of sweat collected from jehoash, noel and mr ryan when playing tetris. 
+effect: increase speed by 10, but decrease vitaility by 5
+*/
+
