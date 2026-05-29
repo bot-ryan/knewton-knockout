e69f8f3554d16c4f2a2061cf26c37a5429b2a5ab
bot-ryan <ryancalipusan0@gmail.com>
2026-05-29 15:58:26 +0800

    edited

--- gladiators/src/data/Relics/NaqeebRelics.ts
+++ gladiators/src/data/Relics/NaqeebRelics.ts
@@ -11,7 +11,9 @@
 
 /*
 Curse of Naquarius [SPELL]
-effect: fart will linger with you for 2 mins will decrease speeed, vitality and strength by 10
+effect: fart will linger with you for 2 mins will 
+decrease agility, 
+vitality and strength by 10
 
 [RELIC] Tetr sweat: a bottle of sweat collected from jehoash, noel and mr ryan when playing tetris. 
 effect: increase speed by 10, but decrease vitaility by 5
